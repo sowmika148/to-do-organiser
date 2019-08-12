6271f8721c9c75744707063d9d3b8cf95235a0e5
sowmika <sowmika148@gmail.com>
2019-08-12 21:31:21 +0530

    Add add task button and search field

--- src/main/TaskScheduler.js
+++ src/main/TaskScheduler.js
@@ -3,7 +3,8 @@ import React from "react";
 function TaskScheduler() {
     return (
         <div className='Task-scheduler-container'>
-
+            <input type="text" className="Search-Box"/>
+            <button className="Button">Add Task</button>
         </div>
     );
 }
--- src/test/TaskScheduler.spec.js
+++ src/test/TaskScheduler.spec.js
@@ -14,4 +14,19 @@ describe('Header Rendering', () => {
         expect(wrapper.find('div')).toHaveLength(1);
         expect(wrapper.find('div').hasClass('Task-scheduler-container')).toBe(true);
     });
+
+    it('should render add task button', function () {
+        const wrapper = shallow(<TaskScheduler/>);
+
+        expect(wrapper.find('button')).toHaveLength(1);
+        expect(wrapper.find('button').contains('Add Task')).toBe(true);
+        expect(wrapper.find('button').hasClass('Button')).toBe(true);
+    });
+
+    it('should render search input box', function () {
+        const wrapper = shallow(<TaskScheduler/>);
+
+        expect(wrapper.find('input')).toHaveLength(1);
+        expect(wrapper.find('input').hasClass('Search-Box')).toBe(true);
+    });
 });
