edd79c9df1b620750658123929e20941c58c0ec3
sowmika <sowmika148@gmail.com>
2019-08-12 19:13:05 +0530

    Remove date picker

--- src/main/TaskScheduler.js
+++ src/main/TaskScheduler.js
@@ -1,12 +1,9 @@
 import React from "react";
-import DatePicker from "react-datepicker";
-import "react-datepicker/dist/react-datepicker.css";
 
 function TaskScheduler() {
     return (
         <div className='Task-scheduler-container'>
-            Select Date:
-            <DatePicker/>
+
         </div>
     );
 }
--- src/test/TaskScheduler.spec.js
+++ src/test/TaskScheduler.spec.js
@@ -3,18 +3,15 @@ import React from "react";
 import * as Enzyme from "enzyme";
 import Adapter from 'enzyme-adapter-react-16';
 import TaskScheduler from "../main/TaskScheduler";
-import DatePicker from "react-datepicker";
 
 Enzyme.configure({ adapter: new Adapter() });
 
 describe('Header Rendering', () => {
 
-    it('should render react Calendar', function () {
+    it('should render task scheduler container', function () {
         const wrapper = shallow(<TaskScheduler/>);
 
         expect(wrapper.find('div')).toHaveLength(1);
         expect(wrapper.find('div').hasClass('Task-scheduler-container')).toBe(true);
-        expect(wrapper.find('div').contains('Select Date:')).toBe(true);
-        expect(wrapper.find(DatePicker)).toHaveLength(1);
     });
 });
